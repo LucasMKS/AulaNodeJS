6503fa426c83c778f49ce813b765a82e29052f1b
Lucas Marques da Silva <111651260+LucasMKS@users.noreply.github.com>
2022-10-22 00:24:27 +0000

    atualização

--- index.js
+++ index.js
@@ -10,14 +10,31 @@ var port = process.env.PORT;
 app.get('/', function(req, res) {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-res.send('Boa noite turma de SD');
+res.send('Boa noite turma de Usabilidade');
 });
+
 //http://127.0.0.1:8030/info
 app.get('/info', function(req, res) {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 res.send('Esta é a página de informação.');
 });
+
+//http://127.0.0.1:8030/index.html
+app.get('/index.html', function(req, res) {
+ res.statusCode = 200;
+ res.setHeader('Content-Type', 'text/html; charset=utf-8');
+ res.send(`<html>
+ <head>
+ <title>Página Inicial</title>
+ </head>
+ <body>
+ <h1>Esta é a Página Inicial</h1>
+ </body>
+ </html>`);
+});
+
+
 app.listen(port, function() {
 console.log(`O servidor foi iniciado`);
 });
